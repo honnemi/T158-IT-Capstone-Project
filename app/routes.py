9e8079410609f7ca8f4215ff59447b081a975bb8
Angelica Siv <angelicasiv3@outlook.com>
2026-08-11 13:43:17 +1000

    added route for itinerary overview

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,7 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def home():
     return render_template("base.html")
+
+@main.route("/itinerary")
+def show_itinerary_overview():
+    return render_template("itinerary-overview.html")
