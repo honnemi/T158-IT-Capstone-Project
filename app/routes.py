ee74e3a9ff269192e302d82ad07235b11ecae438
Airyrys <toothebrick@gmail.com>
2026-08-19 09:52:43 +1000

    fixed home

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ main = Blueprint("main", __name__)
 
 @main.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("home.html")
 
 @main.route("/itinerary")
 def show_itinerary_overview():
