4c14c76d8a428cae667eb34ae81914c1ee3abf94
Angelica Siv <angelicasiv3@outlook.com>
2026-09-07 11:45:21 +1000

    added working reset password flow + complex password enforcement

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import SubmitField, StringField, PasswordField
-from wtforms.validators import InputRequired, Email
+from wtforms.validators import InputRequired, Email, EqualTo, Length, Regexp
 
 class LoginForm(FlaskForm):
     email=StringField("Email", validators=[InputRequired(), Email("Enter email")])
     password=PasswordField("Password", validators=[InputRequired('Enter password')])
     submit = SubmitField("Login")
+
+class ResetPasswordForm(FlaskForm):
+    new_password=PasswordField("New Password", validators=[InputRequired('Enter password'), Length(min=8), Regexp(
+                r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$',
+                message="Password must include at least one uppercase letter, one lowercase letter, one number, and one special character."
+            )])
+    confirm_password=PasswordField("Confirm New Password", validators=[InputRequired('Enter password'), Length(min=8), EqualTo('new_password', message='Passwords must match.'), Regexp(
+                r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$',
+                message="Password must include at least one uppercase letter, one lowercase letter, one number, and one special character."
+            )])
+    submit = SubmitField("Reset")
+    
+    
     
     
